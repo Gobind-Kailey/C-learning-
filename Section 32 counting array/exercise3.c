29a6e9ba7bc9e387d09899219db40bec5bf2ea34
kaileg2 <gobind2975@gmail.com>
2025-05-07 16:28:16 -0400

    Finished Section 32

--- Section 32 counting array/exercise3.c
+++ Section 32 counting array/exercise3.c
@@ -4,22 +4,24 @@
 
 // Samething as before but with uppercase letters
 
-int find_max(int *couterArr)
+// couterArr[i] reps how many times each letter was called "look up table"
+
+int find_max_index(int *couterArr)
 {
     int i;
-    int max = couterArr[0]; 
-    printf("Starting Max = %d\n", max);
+    int max_index = 0; 
+    printf("Starting max_index = %d\n", max_index);
     // 0-9
     for(i = 0; i < 53; i++)
     {
-        if(couterArr[i] > max)
-            max = i;    // max reps the index here 
+        if (couterArr[i] > couterArr[max_index])
+            max_index = i;
         // printf("%c\n", couterArr[i]);
     }
-    if(max >= 0 && max <= 25) printf("Max uppercase= %c\n", max + 'A');
-    else if(max >= 26 && max <= 51) printf("Max lowercase = %c\n", max + 70);
-    else if(max == 52) printf("Max space = ' '");
-    printf("Number of apperences: %d", couterArr[max]);
+    if(max_index >= 0 && max_index <= 25) printf("max_index uppercase = %c\n", max_index + 'A');
+    else if(max_index >= 26 && max_index <= 51) printf("max_index lowercase = %c\n", max_index -26 + 'a');
+    else if(max_index == 52) printf("max_index space = ' '");
+    printf("Number of apperences: %d", couterArr[max_index]);
 }
 
 
@@ -33,13 +35,13 @@ int main()
     {
         if(charArr[i] >= 'A' && charArr[i]<= 'Z') counterArr[charArr[i]-'A']++; // 0-25
 
-        else if(charArr[i] >= 'a' && charArr[i]<= 'z') counterArr[charArr[i]- 70]++; // 26-52
+        else if(charArr[i] >= 'a' && charArr[i]<= 'z') counterArr[charArr[i]- 'a' + 26]++; // 26-52
 
-        else if(charArr[i] == ' ') counterArr[53]++; // 53
+        else if(charArr[i] == ' ') counterArr[52]++; // 52
         
     }
     // printf("Here");
-    find_max(counterArr);
+    find_max_index(counterArr);
 
     return 0; 
 }
